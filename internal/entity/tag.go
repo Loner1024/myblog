e17ed3b52842ceb28331ed0182dcdbbe264edad8
Loner1024 <xth12138@gmail.com>
2022-06-03 20:46:45 +0800

    feat:tag

--- internal/entity/tag.go
+++ internal/entity/tag.go
@@ -1,1 +1,25 @@
 package entity
+
+import (
+	"errors"
+	"github.com/google/uuid"
+)
+
+var (
+	ErrTagEmpty = errors.New("tag not allow empty")
+)
+
+type Tag struct {
+	ID  uuid.UUID
+	Tag string
+}
+
+func NewTag(tag string) (Tag, error) {
+	if tag == "" {
+		return Tag{}, ErrTagEmpty
+	}
+	return Tag{
+		ID:  uuid.New(),
+		Tag: tag,
+	}, nil
+}
---
+++ internal/entity/tag_test.go
@@ -0,0 +1,18 @@
+package entity
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewTag(t *testing.T) {
+	tagValue := "tag"
+	tag, err := NewTag(tagValue)
+	assert.NoError(t, err, ErrTagEmpty)
+	assert.Equal(t, tagValue, tag.Tag)
+}
+
+func TestEmptyTag(t *testing.T) {
+	_, err := NewTag("")
+	assert.ErrorIs(t, err, ErrTagEmpty)
+}
